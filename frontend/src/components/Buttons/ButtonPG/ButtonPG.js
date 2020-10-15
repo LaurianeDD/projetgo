64bccf2ed5dbeb80d8f7d9056ef7f69722d7aa4f
Lauriane <lduchesneauduruisseau@gmail.com>
2020-10-15 14:35:04 -0400

    Add sizing and default value to ButtonPG

--- frontend/src/components/Buttons/ButtonPG/ButtonPG.js
+++ frontend/src/components/Buttons/ButtonPG/ButtonPG.js
@@ -6,12 +6,18 @@ import './ButtonPG.css';
   BUTTON for projet Go
   Takes a string: as text on the button
   Takes a variant: orange or teal
+  Takes a size
   Takes a function: as handleClick
 */
-export default function ButtonPG({text, variant, handleClick}) {
+export default function ButtonPG({text, variant, size, handleClick}) {  
+  //Set defaults values if missing props
+  text = text || 'Button';
+  size = size || 'md';
+  variant = variant || 'orange';
 
   return (
     <Button
+      className={size}
       variant={variant}
       onClick={handleClick}
     >
