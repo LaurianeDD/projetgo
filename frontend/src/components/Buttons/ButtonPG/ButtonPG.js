13cc97279beba07422a55a62fa84095c56d53458
Lauriane <lduchesneauduruisseau@gmail.com>
2020-10-17 19:29:34 -0400

    WIP

--- frontend/src/components/Buttons/ButtonPG/ButtonPG.js
+++ frontend/src/components/Buttons/ButtonPG/ButtonPG.js
@@ -5,23 +5,26 @@ import './ButtonPG.css';
 /*
   BUTTON for projet Go
   Takes a string: as text on the button
-  Takes a variant: orange or teal
-  Takes a size: sm, md or lg
+  Takes a variant: orange(default) or teal
+  Takes a size: sm, md(default) or lg
   Takes a function: as onClick
+  Takes a type: submit, reset, button(default)
 */
-export default function ButtonPG({text, variant, size, onClick}) {  
+export default function ButtonPG({text, children, variant, size, onClick, type}) {  
   //Set defaults values if missing props
   text = text || 'Button';
   size = size || 'md';
   variant = variant || 'orange';
+  type = type || 'button';
 
   return (
     <Button
       className={size}
       variant={variant}
       onClick={onClick}
+      type={type}
     >
-      {text}
+      {text || children}
     </Button>
   );
 }
--- frontend/src/components/Fundraising/Fundraising.js
+++ frontend/src/components/Fundraising/Fundraising.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ButtonPG from '../Buttons/ButtonPG/ButtonPG';
 import DonationsListing from './DonationsListing/DonationsListing';
+import NewFundraising from './NewFundraising/NewFundraising';
 
 export default function Fundraising() {
+  const [currentCampaign, setCurrentCampaign] = useState({});
+  const [showCreateNew, setShowCreateNew] = useState(false);
+
+  const cancelNew = () => {
+    setShowCreateNew(false);
+  }
+  
+  const handleCreateClick = () => {
+    setShowCreateNew(true);
+  };
+  
   //Need the fundraising for this project if exist
-
-  const handleCreateClick = () => {};
-
+  useEffect();
+  
   return (
     <Container>
       <Row>
-        <Col>
-          {/*
-            Current fundraising || 
-            <ButtonPG 
-              text="Créer une campagne"
-              variant="orange"
-              size="lg"
-              onClick={handleCreateClick}
-            >
-          */}
-        </Col>
+        {
+          currentCampaign ?
+          <Campaign /> : 
+          <ButtonPG 
+            text="Créer une campagne"
+            variant="orange"
+            size="lg"
+            onClick={handleCreateClick}
+          />
+        }
       </Row>
+      {
+        showCreateNew ?
+        <Row>
+          <NewFundraising cancelClick={cancelNew} />
+        </Row> :
+        <></>
+      }
       <Row>
         <Col>
           <DonationsListing />
